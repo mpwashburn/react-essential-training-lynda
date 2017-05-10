80c5a2c61afca57150dcccb371bf7a9a2ccccbe3
Michael Washburn <washburn.michael@gmail.com>
2017-05-09 21:06:24 -0700

    refactored to add bundle.js file

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,16 +1,18 @@
+'use strict';
+
 //Everything below is converted to
 // JSX is Javascript XML and is a tag based syntax that we can use to create React elements
-const { render } = ReactDOM
+var _ReactDOM = ReactDOM,
+    render = _ReactDOM.render;
 
-render(
-  <h1 id='title'
-    className='header'
-    style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
-  Hello World
-  </h1>,
-  document.getElementById('react-container')
-)
 
+render(React.createElement(
+  'h1',
+  { id: 'title',
+    className: 'header',
+    style: { backgroundColor: 'orange', color: 'white', fontFamily: 'verdana' } },
+  'Hello World'
+), document.getElementById('react-container'));
 
 //*************************************************************
 // //ES6 destructuring to make code cleaner
