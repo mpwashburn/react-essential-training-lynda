2f851823c5ded0f447578b20b3caa98e6e59fd27
Michael Washburn <washburn.michael@gmail.com>
2017-05-10 22:26:51 -0700

    added webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+//webpack.config describes evething that we want webpack to do to our files to ready them for production.
+var webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: "dist/assets",
+        filename: "bundle.js",
+        publicPath: "assets"
+    },
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        port: 3000
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: ["babel-loader"],
+                query: {
+                    presets: ["latest", "stage-0", "react"]
+                }
+        ]   }
+    }
+}
+
+
